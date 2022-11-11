379172b14d4af46bc1ef07c83acf41e5582400a8
connectriaj <connect.riaj@hotmail.com>
2022-11-11 22:33:42 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA4znbEXzmqs_fvM7DL81-4beG1IqrHeQI",
+  authDomain: "taza-news-deb22.firebaseapp.com",
+  projectId: "taza-news-deb22",
+  storageBucket: "taza-news-deb22.appspot.com",
+  messagingSenderId: "549221917174",
+  appId: "1:549221917174:web:56b41defd41f4b46bc27d7",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
